cf90ad87fb4864cac3e7ea5d67db8e8672c5968d
Evgeniy Dimitriev <dimitriev_ev@mail.ru>
2025-08-07 22:56:05 +0500

    Implement PostViewSet with author assignment and permission checks

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,8 +1,6 @@
+from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken import views
-
-from django.urls import include, path
-
 
 router = routers.DefaultRouter()
 
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import PermissionDenied
 
-# Create your views here.
+from posts.models import Comment, Group, Post
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Вы не можете редактировать чужой пост.')
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Вы не можете удалить чужой пост.')
+        instance.delete()
