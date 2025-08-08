13f7f7b0b86c67c2508e637b01bc50a1139cae59
Evgeniy Dimitriev <dimitriev_ev@mail.ru>
2025-08-08 21:20:40 +0500

    Set up main and nested routers for posts, groups, and comments with token auth endpoint

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,10 +1,18 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 
-router = routers.DefaultRouter()
+from .views import CommentViewSet, GroupViewSet, PostViewSet
+
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='posts')
+router.register('groups', GroupViewSet, basename='groups')
+
+comments_router = DefaultRouter()
+comments_router.register('comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
+    path('posts/<int:post_id>/', include(comments_router.urls)),
 ]
