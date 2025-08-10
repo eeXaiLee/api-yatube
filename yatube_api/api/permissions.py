be9f55c931272ceb9a35ed836193bdd7d9638c11
Evgeniy Dimitriev <dimitriev_ev@mail.ru>
2025-08-10 17:29:01 +0500

    Removed redundant docstring

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -9,16 +9,6 @@ class IsAuthorOrReadOnly(BasePermission):
     """
 
     def has_object_permission(self, request, view, obj) -> bool:
-        """Проверяет права доступа к конкретному объекту.
-
-        Args:
-            request: Запрос, для которого проверяются права.
-            view: View, к которому относится проверка.
-            obj: Объект, к которому проверяются права.
-
-        Returns:
-            bool: True если доступ разрешен, False в противном случае.
-        """
         if request.method in SAFE_METHODS:
             return True
         return obj.author == request.user
